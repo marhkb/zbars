4f7029daaa08e20f90a58b21f4d81229ed5417e4
Marcus Behrendt <marcus.behrendt.86@gmail.com>
2018-05-20 18:01:42 +0200

    SymbolSet tests

--- src/symbolset.rs
+++ src/symbolset.rs
@@ -49,6 +49,14 @@ impl Deref for SymbolSet {
     fn deref(&self) -> &Self::Target { &self.symbol_set }
 }
 
+impl Clone for SymbolSet {
+    fn clone(&self) -> Self {
+        let mut symbol_set = Self { symbol_set: self.symbol_set };
+        symbol_set.set_ref(1);
+        symbol_set
+    }
+}
+
 impl Drop for SymbolSet {
     fn drop(&mut self) { self.set_ref(-1) }
 }
@@ -60,6 +68,15 @@ pub mod zbar_fork {
     impl SymbolSet  {
         pub fn first_symbol_unfiltered(&self) -> Option<Symbol> {
             Symbol::from_raw(unsafe { zbar_symbol_set_first_unfiltered(**self) })
+        }
+    }
+
+    #[cfg(test)]
+    mod test {
+        use super::*;
+
+        #[test] fn test_first_symbol_unfiltered() {
+            assert_eq!(create_symbol_set().first_symbol_unfiltered().unwrap().data(), "Hello World");
         }
     }
 }
@@ -76,7 +93,47 @@ impl Iterator for SymbolIter {
 
     fn next(&mut self) -> Option<Self::Item> {
         let mut next = self.symbol.as_ref().and_then(Symbol::next);
-        ::std::mem::swap(&mut self.symbol, &mut next);
+        mem::swap(&mut self.symbol, &mut next);
         next
     }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test] fn test_size() { assert_eq!(create_symbol_set().size(), 2); }
+    #[test] fn test_first_symbol() {
+        assert_eq!(create_symbol_set().first_symbol().unwrap().data(), "Hello World");
+    }
+    #[test] fn test_iter() {
+        let symbols = create_symbol_set();
+        let mut iter = symbols.iter();
+        assert_eq!(iter.next().unwrap().data(), "Hello World");
+        assert_eq!(iter.next().unwrap().data(), "Hallo Welt");
+        assert!(iter.next().is_none());
+    }
+}
+
+#[cfg(test)]
+fn create_symbol_set() -> SymbolSet {
+    create_symbol_from("test/greetings.png").symbols().unwrap()
+}
+#[cfg(test)]
+fn create_symbol_from(path: impl AsRef<::std::path::Path>) -> prelude::ZBarImage<'static> {
+    use prelude::{
+        ZBarImage,
+        ImageScanner
+    };
+
+    let mut image = ZBarImage::from_path(&path).unwrap();
+
+    let mut scanner = ImageScanner::builder()
+        .with_cache(false)
+        .with_config(ZBarSymbolType::ZBAR_QRCODE, ZBarConfig::ZBAR_CFG_ENABLE, 1)
+        .with_config(ZBarSymbolType::ZBAR_CODE128, ZBarConfig::ZBAR_CFG_ENABLE, 1)
+        .build()
+        .unwrap();
+    scanner.scan_image(&mut image).unwrap();
+    image
 }
